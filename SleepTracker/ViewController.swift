44188cf03eb88978afa289033822d7b5468c7a1c
Drew Leary <cupcake.drew@gmail.com>
2016-04-12 10:23:21 -0700

    Initial Commit

---
+++ SleepTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepTracker
+//
+//  Created by Drew Leary on 4/12/16.
+//  Copyright © 2016 Drew Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
