dbaec4a318d4f9e24359afe0aa1c910ec510eb47
Drew Leary <cupcake.drew@gmail.com>
2016-04-20 10:52:25 -0700

    Initial commit

---
+++ SleepTracker/SleepTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SleepTableViewCell.swift
+//  SleepTracker
+//
+//  Created by Drew Leary on 4/13/16.
+//  Copyright © 2016 Drew Leary. All rights reserved.
+//
+
+import Cocoa
+
+class SleepTableViewCell: UITableViewCell {
+
+}
